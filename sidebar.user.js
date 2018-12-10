4db1ee55c402f0bc4c56a449ebef2ad5ed22af64
Jace <jace.benson@protonmail.com>
2018-12-10 13:07:32 -0600

    Create sidebar.user.js

---
+++ sidebar.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Hide Sidebar
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  This script hides the sidebar on community.servicenow.com
+// @author       You
+// @match        https://community.servicenow.com/*
+// @grant        none
+// @updateURL https://openuserjs.org/meta/jacebenson/Hide_Sidebar.meta.js
+// @copyright 2018, jacebenson (https://openuserjs.org//users/jacebenson)
+// @license MIT
+// ==/UserScript==
+/* global $ */
+(function() {
+    'use strict';
+    var checkExist = setInterval(function() {
+        if ($('sp-page-row').length) {
+            console.log("Exists!");
+            $($('sp-page-row > .row >.col-md-9')[0]).attr('class','col-md-12');
+            $($('sp-page-row > .row >.col-md-3')[0]).attr('class','hide');
+            clearInterval(checkExist);
+        }
+    }, 1000); // check every 1s
+})($);
