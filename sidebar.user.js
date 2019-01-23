08197947631e6876f96972ba5841678cfb125f17
Jace <jace.benson@protonmail.com>
2019-01-23 11:03:28 -0600

    Update sidebar.user.js

--- sidebar.user.js
+++ sidebar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Wide Servicenow Community
 // @namespace    https://github.com/jacebenson/openuserjs-community-servicenow-com/
-// @version      0.2.4
+// @version      0.2.5
 // @description  This script hides the sidebar on community.servicenow.com
 // @author       Jace
 // @match        https://community.servicenow.com/*
@@ -13,15 +13,32 @@
 /* global $ */
 (function() {
     'use strict';
-    var checkExist = setInterval(function() {
-        if ($('sp-page-row').length) {
-            console.log("Hiding Sidebar!");
-            $($('sp-page-row > .row >.col-md-9')[0]).attr('class','col-md-12');
-            $($('sp-page-row > .row >.col-md-3')[0]).attr('class','hide');
-            $($('#x1c8e106f535032007a97e192d5dc3432')[0]).attr('class','hide');
-            $($('.cm-homepage-main-container > .container')[0]).attr('class', 'container-fluid');
-            clearInterval(checkExist);
+    var removeSideBar = setInterval(function() {
+        try{
+            if ($('sp-page-row').length) {
+                console.log("Hiding Sidebar!");
+                //$($('sp-page-row > .row >.col-md-9')[0]).attr('class','col-md-12');
+                //$($('sp-page-row > .row >.col-md-3')[0]).attr('class','hide');
+                $($('#x1c8e106f535032007a97e192d5dc3432')[0]).attr('class','hide');
+                $($('.cm-homepage-main-container > .container')[0]).attr('class', 'container-fluid');
+                clearInterval(removeSideBar);
+            }
+        } catch(e){
+            console.log(e);
+        }
+    }, 3000); // check every 1s
+    var loadMore = setInterval(function() {
+        try{
+            console.log("Loading More Records");
+            $('.cm-list-pagination > button').click();
+        } catch(e){
+            console.log(e);
         }
     }, 1000); // check every 1s
-    $('#unreplied').click();
+    setTimeout(function () {
+        $('#unreplied').click();
+    }, 2000);
+    setTimeout(function (){
+        clearInterval(loadMore);
+    },10000);
 })($);
